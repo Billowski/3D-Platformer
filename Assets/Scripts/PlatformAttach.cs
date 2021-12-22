e9c237a5b34dc66ee6533b2d4f4ae7315dc5b063
Billu <Billowski98@gmail.com>
2021-12-22 10:41:24 +0100

    Project started

---
+++ Assets/Scripts/PlatformAttach.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformAttach : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
+    }
+}
